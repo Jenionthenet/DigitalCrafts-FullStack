b7b769d243fdcdc83f9b574dd14869cb9e31dda9
Jeni Ine <jbscott_5@yahoo.com>
2021-07-09 16:43:26 -0400

    todo list without delet function

---
+++ todo_list_dictionaries.py
@@ -0,0 +1,35 @@
+tasks = []
+
+while True:
+
+    print("1 to Add Tasks")
+    print("2 to Delete Tasks")
+    print("3 to View All Tasks")
+    print("q to Quit ")
+
+    option = (input("Enter your option number: "))
+
+
+
+    if option == "1":
+        title = input("Enter your task: ")
+        priority = input("Enter the task priority level 'high', 'medium', or 'low': ")
+        task_dict = {"title" : title, "priority" : priority}
+        tasks.append(task_dict)
+        print(task_dict)
+
+    elif option == "3":
+        i = 1
+        for task in tasks:
+            print(f"{i} - {task['title']} - {task['priority']}")
+            i += 1
+        
+    
+    elif option == "q":
+        break
+
+
+
+
+
+
