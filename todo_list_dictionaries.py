1691668f65220e69a4389009484a4289b967de49
Jenifer Ine <jbscott_5@yahoo.com>
2021-07-11 14:34:14 -0400

    created a function to list out tasks

--- todo_list_dictionaries.py
+++ todo_list_dictionaries.py
@@ -1,40 +1,44 @@
 tasks = []
 
+def view_task_list(lines):
+    i = 1
+    for task in tasks:
+        print(f"{i} - {task['title']} - {task['priority']}")
+        i += 1
+
 while True:
 
+    print ("")
     print("1 to Add Tasks")
     print("2 to Delete Tasks")
     print("3 to View All Tasks")
     print("q to Quit ")
+    print("")
 
-    option = (input("Enter your option number: "))
+    option = (input("Enter your option: "))
 
+    if option == "q":
+            break
 
-
-    if option == "1":
+    elif option == "1":
         title = input("Enter your task: ")
         priority = input("Enter the task priority level 'high', 'medium', or 'low': ")
         task_dict = {"title" : title, "priority" : priority}
         tasks.append(task_dict)
-        print(task_dict)
+        #print(task_dict)
 
     elif option == "2":
-        i = 1
-        for task in tasks:
-            print(f"{i} - {task['title']} - {task['priority']}")
-            i += 1
+        view_task_list(tasks)
+        print("")
         deleted_task = int(input("Enter the task number that you wish to delete: "))
         del tasks[deleted_task -1]
 
     elif option == "3":
-        i = 1
-        for task in tasks:
-            print(f"{i} - {task['title']} - {task['priority']}")
-            i += 1
+        view_task_list(tasks)
         
     
-    elif option == "q":
-        break
+    else:
+        print("Please enter a choice from the menu.")
 
 
 
