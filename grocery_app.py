db1e82a8ba489af849d175a4e5552fb50108ef2e
Jeni Ine <jbscott_5@yahoo.com>
2021-07-13 17:20:13 -0400

    added delete options for stores and items

--- grocery_app.py
+++ grocery_app.py
@@ -1,10 +1,4 @@
-shopping_list = []
-
-"""def view_shopping_lists(arr):
-    i = 1
-    for index in range(0, len(arr)):
-        store = arr[index]
-        print(f"{i} - store name: {store.title},   store address: {store.address}")"""
+shopping_lists = []
 
    
 class ShoppingList:
@@ -27,49 +21,85 @@ while True:
     print("1 to Add a store to your shopping list")
     print("2 to Add items to your shopping list")
     print("3 to View your shopping lists")
-    print("q to Quit")
+    print("4 to delete a store from your shopping list")
+    print("5 to delete items from your shopping list")
+    print("6 to Quit")
     print("")
-
-    option = input("Enter your option: ")
-
+    try:
+        option = int(input("Enter your option: "))
     
-    if option == "1":
-        store_name = input("Enter store name: ")
-        store_addy = input("Enter store address: ")
-        store = ShoppingList(store_name, store_addy)
-        shopping_list.append(store)
-
-    if option == "2":
-      
-
-        for i in range(0, len(shopping_list)):
-            print(f"{i + 1} - {shopping_list[i].title}")
-            store = shopping_list[i]
-        store_index = int(input("Enter the store number: "))
-        item_name = input("Enter the name of the item: ")
-        price = float(input("Enter the price: "))
-        quantity = int(input("Enter the quantity: "))
-        thing = Items(item_name, price, quantity)
-        
-        shopping_list[store_index -1].items.append(thing)
-      
-     
-
-    if option == "3":
-        i = 1
-        for index in range(0, len(shopping_list)):
-            store = shopping_list[index]
-            print(f"{i} - store name: {store.title},   store address: {store.address}")
-            i += 1
-            for index in range(0, len(store.items)):
-                title = store.items[index].title
-                quantity = store.items[index].quantity 
-                print(title, quantity)
     
-        
-       
-    if option == "q":
-        break
+        if option == 1:
+            store_name = input("Enter store name: ")
+            if store_name == "":
+                print("Please enter store name")
+            store_address = input("Enter store address: ")
+            if store_address == "":
+                print("Please enter store address")
+            
 
-    else: 
-        print("Please enter a choice from the menu.")
+            store = ShoppingList(store_name, store_address)
+            shopping_lists.append(store)
+
+        elif option == 2:
+        
+
+            for i in range(0, len(shopping_lists)):
+                print(f"{i + 1} - {shopping_lists[i].title}")
+                store = shopping_lists[i]
+            store_index = int(input("Enter the store number: "))
+            item_name = input("Enter the name of the item: ")
+            price = float(input("Enter the price: "))
+            quantity = int(input("Enter the quantity: "))
+            thing = Items(item_name, price, quantity)
+            
+            shopping_lists[store_index -1].items.append(thing)
+        
+        
+
+        elif option == 3:
+            i = 1
+            for index in range(0, len(shopping_lists)):
+                store = shopping_lists[index]
+                print(f"{i} {store.title}")
+                i += 1
+                for index in range(0, len(store.items)):
+                    title = store.items[index].title
+                    quantity = store.items[index].quantity 
+                    print(f" - {title} ({quantity})")
+        
+        elif option == 4:
+            for i in range(0, len(shopping_lists)):
+                print(f"{i + 1} - {shopping_lists[i].title}")
+                store = shopping_lists[i]
+            delete_store = int(input("Enter the store number that you wish to delete: "))
+            del shopping_lists[delete_store - 1]
+
+        elif option == 5:
+            i = 1
+            for index in range(0, len(shopping_lists)):
+                store = shopping_lists[index]
+                print(f"{i} {store.title}")
+                i += 1
+                x = 1
+                for index in range(0, len(store.items)):
+                    title = store.items[index].title
+                    quantity = store.items[index].quantity 
+                    print(f"   {x} - {title} ({quantity})")
+                    x += 1
+            delete_item_store = int(input("Enter the store number that you wish to delete an item from: "))
+            store = shopping_lists[delete_item_store - 1]
+            for i in range(0, len(store.items)):
+                print(f"{i + 1} {store.items[i].title}")
+
+            delete_item = int(input("Enter the number of the item that you wish to delete: "))
+            del store.items[delete_item - 1]
+
+
+
+        
+        elif option == 6:
+            break
+
+    except ValueError:
+        print("Invalid input. Please enter a number, not a letter: ")
